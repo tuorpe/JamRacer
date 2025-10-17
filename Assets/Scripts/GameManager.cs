6d42ba3a4c96ac94a2db002a9e7cefe71d3a6343
AnttiN <fanno004@edu.xamk.fi>
2025-10-17 11:39:52 +0300

    Required scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    [Header("Game State Variables")]
+    [SerializeField] public bool isPlaying = false;
+    [SerializeField] public bool isPaused = false;
+    [SerializeField] public bool isGameOver = false;
+    [SerializeField] public bool isMainMenu = true;
+    [SerializeField] public bool isInGameMenu = false;
+
+    [Header("Player Stats")]
+    [SerializeField] public float gameTime = 0f;
+    [SerializeField] public int score = 0;
+    [SerializeField] public int lives = 3;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
